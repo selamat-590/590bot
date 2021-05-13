9bbf2e7c6663bd2e2d47a52587ac0c6ee6c722ac
selamat-590 <81462708+selamat-590@users.noreply.github.com>
2021-05-13 19:50:42 +0700

    Add files via upload

---
+++ language/nad.js
@@ -0,0 +1,156 @@
+exports.wait = () => {
+	return`*「❗」sedang di proses...*`
+}
+
+exports.succes = () => {
+	return`*「 SUKSES KAK」*`
+}
+
+exports.lvlon = () => {
+	return`*「❗」LEVELING DIAKTIFKAN*`
+}
+
+exports.lvloff = () => {
+	return`*「❗」LEVELING DIMATIKAN*`
+}
+
+exports.lvlnul = () => {
+	return`*「❗」LEVELMU MASIH KOSONG*`
+}
+
+exports.lvlnoon = () => {
+	return`*「❗」LEVEL DI GRUB BELUM DI AKTIFKAN*`
+}
+
+exports.noregis = () => {
+	return`*「❗」BELUM TERDAFTAR「❗」*\n\nsilakan daftar ketik : @verify`
+}
+
+exports.baned = () => {
+	return`*「❗」Maaf kak kamu telah di banned , hubungi owner:v*`
+}
+
+exports.premium = (prefix) => {
+	return`Fitur ini khusus user premium!
+Upgrade ke premium dulu kak
+Jika minat hubungi owner ku
+Ketik : ${prefix}owner`
+}
+
+exports.rediregis = () => {
+	return`*Kamu sudah melakukan verify -_-*`
+}
+
+exports.stikga = () => {
+	return`*「 GAGAL 」Coba reply/tag ulang kak*`
+}
+
+exports.linkga = () => {
+	return`*「❗」maaf linknya tidak valid kak*`
+}
+
+exports.groupo = () => {
+	return`*「❗」GROUP ONLY*`
+}
+
+exports.ownerb = () => {
+	return`*「❗」OWNER BOT ONLY*`
+}
+
+exports.ownerg = () => {
+	return`*「❗」OWNER GROUP ONLY*`
+}
+
+exports.admin = () => {
+	return`*「❗」ADMIN GROUP ONLY*`
+}
+
+exports.badmin = () => {
+	return`*「❗」BOT HARUS JADI ADMIN*`
+}
+
+exports.bug = () => {
+	return`*Masalah telah di laporkan ke owner Lexxy, laporan palsu/main2 tidak akan ditanggapi*`
+}
+
+exports.wrongf = () => {
+	return`*「🗿」Teks nya mana kak?*`
+}
+
+exports.clears = () => {
+	return`*Sukses bosku*`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel) => {
+	return`
+*「 SELAMAT 」*
+\`\`\`➸ Nama : ${pushname}\`\`\`
+\`\`\`➸ Nomor : wa.me/${sender.split("@")[0]}\`\`\`
+\`\`\`➸ Xp : ${getLevelingXp(sender)}\`\`\`
+\`\`\`➸ Level : ${getLevel} ➸ ${getLevelingLevel(sender)}\`\`\`
+`}
+ 
+exports.limitend = (pushname, prefix) => {
+	return`*maaf ${pushname} limit hari ini habis*
+*Limit di reset setiap jam 24:00*
+
+\`\`\`Upgrade Premium Bebas Limit\`\`\`
+\`\`\`➸ 10K : 1 Bulan\`\`\`
+\`\`\`Jika berminat silahkan hubungi owner\`\`\`
+\`\`\`Kirim perintah : ${prefix}owner\`\`\`
+`
+}
+
+exports.limitcount = (isPrem, limitCounts) => {
+	return`
+*「 LIMIT COUNT 」*
+sisa limit anda : ${isPrem ? '99999' : `${limitCounts}`}
+
+Upgrade premium bosku, biar bebas gunain bot`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`┏━━━━━━━♡ *ATM* ♡━━━━━━━┓
+┃╭───────────────────
+┃│➸ NAMA : ${pushname}
+┃│➸ NOMOR : ${sender.split("@")[0]}
+┃│➸ UANG : ${uangkau}
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛`
+}
+
+exports.donasi = () => {
+return`*DONASI KAK*
+Pulsa : 0857-8900-4732
+Ovo : 0822-7991-5237
+Dana : 0822-7991-5237
+Follow IG : https://instagram.com/ff.lexxy._?igshi
+
+Donasi supaya bot terus update & Aktif`
+}
+
+exports.iklan = (botName, ownerNumbers, ownerName) => {
+return`🔰 -----[ 「 *IKLAN ${botName}* 」 ]----- 🔰
+──────────────────────────────
+◯ *DAFTAR SEWA & BUAT BOT :*
+◯ *SEWA : 15K/GRUP (BULAN)*
+◯ *BUAT : 70K (BISA JADI OWNER)*
+◯ *PEMBAYARAN BISA MELALUI :*
+◯ *OVO, GOPAY, DANA, PULSA+10K*
+──────────────────────────────
+◯ *KEUNTUNGAN SEWA BOT :*
+◯ *1. BISA MEMASUKAN BOT KE GROUP*
+◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*
+◯ *KEUNTUNGAN BUAT BOT :*
+◯ *1. BISA MENJADI OWNER BOT SENDIRI*
+◯ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+◯ *3. BISA MEMBAWA BOT KE GROUP*
+◯ *4. BISA MENGGUNAKAN COMMAND OWNER*
+◯ *5. BISA MENYEWAKAN BOT KEMBALI*
+──────────────────────────────
+◯ *JIKA MINAT IKLAN DIATAS*
+◯ *HARAP HUBUNGI NOMOR DIBAWAH :*
+◯ *wa.me/${ownerNumbers}*
+──────────────────────────────
+🔰 -----[「 *POWERED BY ${ownerName}* 」]----- 🔰`
+}
